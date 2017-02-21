216e44c3d2427dddb113fdce84f51c761d231bef
Danya D. Smith <danyadsmith@gmail.com>
2017-02-20 23:15:55 -0500

    Display Chat Messages and Room Select Menu
    Implements a setInterval function that sends a GET request to the
    chatterbox api and returns a results object.
    
    Implements displayMessages as the success action. This function
    currently:
    
    * Iterates through all chat messages and appends them to the DOM
    * Checks for unique room names and appends them to the drop-down
      list

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,55 @@
 // YOUR CODE HERE:
+
+/* $.ajax({
+ url: "test.html",
+ context: document.body
+ })*/
+
+setInterval(function(){
+  $.ajax({
+    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    dataType: 'json',
+    success: function (results) {
+      displayMessages(results);
+    }
+  }) ;
+}, 1000);
+
+var uniqueRooms = {};
+
+var displayMessages = function (obj) {
+  console.log(obj);
+  var results = obj.results;
+  var chats = $('#chats');
+  var $roomList = $('#roomList');
+
+  for(var i=0; i<results.length; i++) {
+    var currentRoom = results[i].roomname;
+
+    if (uniqueRooms[currentRoom] === undefined) {
+      uniqueRooms[currentRoom] = currentRoom;
+      var $options = $('<option></option>');    // <option value="volvo">Volvo</option>
+      $options.attr('value', currentRoom);
+      $options.text(currentRoom);
+      $options.appendTo($roomList);
+    }
+
+
+    var $messageContainer = $('<article class="chat"></article>');
+
+    var username = results[i].username;
+    var message = results[i].text;
+    var $username = $('<p class="username"></p>');
+    var $message = $('<p></p>');
+    $username.appendTo($messageContainer);
+    $message.appendTo($messageContainer);
+    $username.text(username);
+    $message.text(message);
+    $messageContainer.appendTo(chats);
+
+  }
+
+};
+
+
